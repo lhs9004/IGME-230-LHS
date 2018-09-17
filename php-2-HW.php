e3b4ebef01d658d99e85d616fbb9b432d9c084b7
lhs9004 <42792159+lhs9004@users.noreply.github.com>
2018-09-16 21:56:25 -0400

    Add files via upload
    All of the files in my 230 folder

---
+++ php-2-HW.php
@@ -0,0 +1,8 @@
+<?PHP
+	define("PI", 3.14159);
+	$radius = 10;
+	$area = PI * $radius * $radius;
+    $circumference = PI * $radius * 2;
+   echo "<p>The area of a radius $radius circle is $area</p>";
+   echo "<p>The circumference of a radius $radius circle is $circumference</p>";
+?>
